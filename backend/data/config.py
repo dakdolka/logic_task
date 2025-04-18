9cb1f724f4349b87444b47009abd42e0035c14f5
DolinovAK <dolinovak@mai.ru>
2025-04-18 19:39:09 +0300

    be

--- backend/data/config.py
+++ backend/data/config.py
@@ -7,15 +7,15 @@ class Settings(BaseSettings):
     # user: str
     # password: str
     # db_port: str
-    MYSQL_ROOT_PASSWORD: str
-    MYSQL_DATABASE: str
-    MYSQL_USER: str
-    MYSQL_PASSWORD: str
+    # MYSQL_ROOT_PASSWORD: str
+    # MYSQL_DATABASE: str
+    # MYSQL_USER: str
+    # MYSQL_PASSWORD: str
     
     @property
     def db_url(self):
-        # return f"mysql+asyncmy://dak:200209318Dak()@db:3306/papa_task"
-        return f"mysql+asyncmy://{self.user}:{self.password}@{self.db_host}:{self.db_port}/{self.db_name}"
+        return f"mysql+asyncmy://dak:200209318Dak()@db:3306/papa_task"
+        # return f"mysql+asyncmy://{self.user}:{self.password}@{self.db_host}:{self.db_port}/{self.db_name}"
     
     class Config:
         #в родительской папке лежит .env, исправь
