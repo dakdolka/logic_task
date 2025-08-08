df5a296a74f72c2ee5b4b348cd0d1994ebdfb7a4
DolinovAK <dolinovak@mai.ru>
2025-08-08 18:21:43 +0300

    lll

--- frontend/script.js
+++ frontend/script.js
@@ -159,7 +159,7 @@ async function getResultFile() {
 
 async function login(username, password) {
   try {
-    const response = await fetch('http://backend:8000/api/login', {
+    const response = await fetch('http://localhost:8000/api/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
